db8dda6cd10efab2dbff331daa48cc582638790a
timothythehuman <timothythehuman@gmail.com>
2014-03-22 19:41:13 -0500

    add exception notifier

--- spec/factories/community_categories.rb
+++ spec/factories/community_categories.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :community_category do
     name "Community Category"
 
-    factory :invalid_community do
+    factory :invalid_community_category do
       name nil
     end
   end
