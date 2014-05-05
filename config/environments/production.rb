7409877926c80c1456ce966a73e9f4db7f553049
timothythehuman <timothythehuman@gmail.com>
2014-05-05 13:06:45 -0500

    explicit errors in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ CivicSourcing::Application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.consider_all_requests_local = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
